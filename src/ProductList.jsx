da3717cbfa644d315325fe5f835ed359eba2ffcc
mandarkarjol <mandar.karjol@gmail.com>
2024-08-06 23:10:08 -0400

    fixes bug with useDispatch import

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,7 +2,7 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import addItem from './CartSlice';
-import useDispatch from 'react-redux';
+import { useDispatch } from 'react-redux';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
