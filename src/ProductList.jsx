fb5ba5c10d17901ac40ab0cbb3f16828fd7f4850
mandarkarjol <mandar.karjol@gmail.com>
2024-08-08 22:24:11 -0400

    Fixes add to cart bug caused by improper import of cartSlice

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,8 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
-import addItem from './CartSlice';
+import { addItem } from './CartSlice';
 import { useDispatch } from 'react-redux';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
